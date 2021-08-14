985de93d7c3a26e5b6d17e6d2410bbe8784f8fa8
Abhishek Singh <abhisheks8452@gmail.com>
2021-08-14 12:14:46 +0530

    commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,43 +1,29 @@
 import React from 'react'
-import {Navbar,Container,Nav,NavDropdown,Button} from "react-bootstrap"
-import {Link} from "react-router-dom"
+import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap"
+import { Link } from "react-router-dom"
 import "./Header.css"
 import a from "../images/a.jpg"
 const Header = () => {
-    return (
-        <div>
-          <>
-  
- 
-  <Navbar bg="light">
-  <Container>
-    <Navbar.Brand href="#home">
-      <img
-       
-       width="150"
-       height="120"
-       src={a}
-        className="d-inline-block align-top"
-        alt=""
-      />
-      
-    </Navbar.Brand>
-    
-    <Nav className="font-weight-bold h4 mx-4">
-      <Nav.Link href="#home" className="mx-3">HOME</Nav.Link>
-      <Nav.Link href="#features"className="mx-3">QUESTIONSARRE</Nav.Link>
-      <Nav.Link href="#pricing"className="mx-3">PROFILE</Nav.Link>
-      <Nav.Link href="#pricing"className="mx-3">LOGIN</Nav.Link>
-    </Nav>
-   
-  </Container>
-      
-  
-  </Navbar>
-  
-</>
-        </div>
-    )
+  return (
+    <div class="row">
+      <div class="column">
+        <img
+
+          width="150"
+          height="120"
+          src={a}
+          className="d-inline-block logo"
+          alt=""
+        />
+      </div>
+      <div class="column column1">
+      <a href="HOME">HOME</a>
+      <a href="QUESTIONSARRE">QUESTIONSARRE</a>
+      <a href="PROFILE">PROFILE</a>
+      <a href="LOGIN">LOGIN</a>
+      </div>
+    </div>
+  )
 }
 
 export default Header
